3da02cfe142b970079ab74c8dfd1bccd2ad2a505
Jijin13 <am037selvajijin@gmail.com>
2024-07-15 15:32:00 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, url_for, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
+
+app = Flask(__name__)
+app.secret_key = 'secretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+class Enroll(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    rollnumber = db.Column(db.Integer, unique=True, nullable=False)
+    enrolled_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Data %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        student_name = request.form['name']
+        student_rollnumber = request.form['rollnumber']
+
+        existing_data = Enroll.query.filter_by(rollnumber=student_rollnumber).first()
+        if existing_data:
+            return "Roll number already exist"
+        if student_name is not str:
+            return "Enter a valid Student name"
+        
+        new_data = Enroll(name=student_name, rollnumber=student_rollnumber)
+
+        try:
+            db.session.add(new_data)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your data'
+    else:
+        datas = Enroll.query.order_by(Enroll.enrolled_date).all()
+        return render_template('index.html', datas=datas)
+    
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    data = Enroll.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        data.name = request.form['name']
+        data.rollnumber = request.form['rollnumber']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('edit.html', data=data)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    data = Enroll.query.get_or_404(id)
+
+    try:
+        db.session.delete(data)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem in deleting that data'
+
+if __name__ == '__main__':
+    app.run(debug=True)
